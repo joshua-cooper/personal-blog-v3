dc571851a07e09141fb05892408c00f11e63648e
Joshua Cooper <joshua@cooper.cx>
2019-11-27 14:02:48 +0900

    Added default site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "JocoBlog",
     description:
-      "Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.",
+      "Joshua Cooper's personal technology blog. I like to write about programming, devops, and technology in general. Rust, JavaScript, TypeScript, React, Docker.",
     author: "Joshua Cooper",
   },
   plugins: [
